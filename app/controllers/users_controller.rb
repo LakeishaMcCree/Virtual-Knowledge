08ec1393587e74fb2b04efc4a0924331f3c9adaa
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2020-09-06 14:10:50 -0400

    created the comments views and created home controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
     before_action :must_be_admin
 
-    def index
-        @users = User.all 
+    def new
+        @users = User.new
     end
+
+    def create
+        @user = User.new(user_params)
+       if  
+         @user.save
+            session[:user_id] = @user.user_id
+            redirect_to root_path
+       else
+        render :new
+       end
+    end
+
+
 
     private
 
@@ -12,4 +25,9 @@ class UsersController < ApplicationController
             redirect_to meetings_path, alert: "You don't have access to this page."
         end
     end
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+    
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+    belongs_to :meeting
+    belongs_to :user
+    belongs_to :task 
 end
